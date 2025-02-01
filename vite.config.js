367b1c415bb2351ca77555198b60b13e5f1eebb7
leihuazhe <295482300@qq.com>
2025-02-01 16:37:24 +0900

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import vue from '@vitejs/plugin-vue';
 import eslint from '@rollup/plugin-eslint';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path'; // Import path module
 import { VitePWA } from 'vite-plugin-pwa';
 
 /**
  * @type {import('vite').UserConfig}
  */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default {
   plugins: [
     vue(),
@@ -26,7 +31,7 @@ export default {
   ],
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src'),
+      '~': path.resolve(__dirname, './src'), // Now using path correctly
     },
   },
   build: {
